ea53b6abe3948edc7d094e822e1b6eb30b38dde1
xin-huang <xinhuang.res@gmail.com>
2022-11-09 04:28:24 -0500

    Update tests

--- tests/test_get_quantile.py
+++ tests/test_get_quantile.py
@@ -13,14 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import pytest
 from sstar.get_quantile import get_quantile
+
 
 @pytest.fixture
 def data():
     pytest.model = "./examples/models/BonoboGhost_4K19_no_introgression.yaml"
     pytest.exp_quantile = "./tests/results/test.quantile.exp.summary"
 
+
+@pytest.mark.skip()
 def test_get_quantile(data):
     get_quantile(model=pytest.model, ms_dir='./ext/msdir', seeds=[1,2,3], N0=1000, nsamp=22, nreps=20000, ref_index=4, ref_size=20, tgt_index=3, tgt_size=2, mut_rate=1.2e-8, rec_rate=0.7e-8, seq_len=40000, snp_num_range=[25,30,5], output_dir='./tests/results/simulation', thread=2)
     f1 = open('./tests/results/simulation/quantile.summary.txt', 'r')
