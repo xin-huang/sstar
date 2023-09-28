3431ea5e24dc870f10d0e12598ba98055720b4c2
xin-huang <xinhuang.res@gmail.com>
2023-09-28 19:54:21 +0200

    Refactor __main__.py

--- sstar/__main__.py
+++ sstar/__main__.py
@@ -49,7 +49,15 @@ def _run_tract(args):
     get_tract(threshold_file=args.threshold, match_pct_files=args.match_pct, output_prefix=args.output, diff=args.diff)
 
 
-def _run_archie(args):
+def _run_simulation(args):
+    pass
+
+
+def _run_training(args):
+    pass
+
+
+def _run_inference(args):
     pass
 
 
@@ -101,70 +109,78 @@ def _s_star_cli_parser():
     subparsers.required = True
 
     # Arguments for score subcommand
-    parser = subparsers.add_parser('score', help='calculate S* scores from VCF files')
-    parser.add_argument('--vcf', type=str, dest='vcf', required=True, help='name of the VCF file containing genotypes from samples')
-    _add_ref_ind_args(parser)
-    _add_tgt_ind_args(parser)
-    _add_common_args(parser)
-    _add_window_args(parser)
-    parser.add_argument('--match-bonus', type=int, dest='match_bonus', default=5000, help='bonus for matching genotypes of two different variants; default: 5000')
-    parser.add_argument('--max-mismatch', type=int, dest='max_mismatch', default=5, help='maximum genotype distance allowed; default: 5')
-    parser.add_argument('--mismatch-penalty', type=int, dest='mismatch_penalty', default=-10000, help='penalty for mismatching genotypes of two different variants; default: -10000')
-    parser.set_defaults(runner=_run_score)
+    #parser = subparsers.add_parser('score', help='calculate S* scores from VCF files')
+    #parser.add_argument('--vcf', type=str, dest='vcf', required=True, help='name of the VCF file containing genotypes from samples')
+    #_add_ref_ind_args(parser)
+    #_add_tgt_ind_args(parser)
+    #_add_common_args(parser)
+    #_add_window_args(parser)
+    #parser.add_argument('--match-bonus', type=int, dest='match_bonus', default=5000, help='bonus for matching genotypes of two different variants; default: 5000')
+    #parser.add_argument('--max-mismatch', type=int, dest='max_mismatch', default=5, help='maximum genotype distance allowed; default: 5')
+    #parser.add_argument('--mismatch-penalty', type=int, dest='mismatch_penalty', default=-10000, help='penalty for mismatching genotypes of two different variants; default: -10000')
+    #parser.set_defaults(runner=_run_score)
 
     # Arguments for quantile subcommand
-    parser = subparsers.add_parser('quantile', help='calculate quantiles for S* scores from simulated data without introgression')
-    parser.add_argument('--model', type=str, required=True, help='demographic model without introgression for simulation in Demes YAML format')
-    parser.add_argument('--ms-dir', type=str, dest='ms_dir', required=True, help='directory for the ms program for simulation')
-    parser.add_argument('--N0', type=int, required=True, help='N0 used in ms simulation')
-    parser.add_argument('--nsamp', type=int, required=True, help='sample size (haploid) used in ms simulation')
-    parser.add_argument('--nreps', type=int, required=True, help='number of replicates used in ms simulation')
-    parser.add_argument('--seeds', type=int, nargs=3, default=None, help='three random seed numbers used in ms simulation; default: None')
-    parser.add_argument('--ref-index', type=int, dest='ref_index', required=True, help='index of the reference population in the demographic model (start from 1)')
-    parser.add_argument('--ref-size', type=int, dest='ref_size', required=True, help='sample size (haploid) of the reference population')
-    parser.add_argument('--tgt-index', type=int, dest='tgt_index', required=True, help='index of the target population in the demographic model (start from 1)')
-    parser.add_argument('--tgt-size', type=int, dest='tgt_size', required=True, help='sample size (haploid) of the target population')
-    parser.add_argument('--mut-rate', type=float, dest='mut_rate', required=True, help='mutation rate per generation per base')
-    parser.add_argument('--rec-rate', type=float, dest='rec_rate', required=True, help='recombination rate per generation per base')
-    parser.add_argument('--seq-len', type=int, dest='seq_len', required=True, help='length of simulated sequence')
-    parser.add_argument('--snp-num-range', type=int, dest='snp_num_range', nargs=3, required=True, help='range of SNP numbers in ms simulation; the first parameter is the minimum SNP number, the second parameter is the maximum SNP number, the third parameter is the step size')
-    parser.add_argument('--output-dir', type=str, dest='output_dir', required=True, help='directory for the output files')
-    parser.add_argument('--thread', type=int, default=1, help='number of thread')
-    parser.set_defaults(runner=_run_quantile)
+    #parser = subparsers.add_parser('quantile', help='calculate quantiles for S* scores from simulated data without introgression')
+    #parser.add_argument('--model', type=str, required=True, help='demographic model without introgression for simulation in Demes YAML format')
+    #parser.add_argument('--ms-dir', type=str, dest='ms_dir', required=True, help='directory for the ms program for simulation')
+    #parser.add_argument('--N0', type=int, required=True, help='N0 used in ms simulation')
+    #parser.add_argument('--nsamp', type=int, required=True, help='sample size (haploid) used in ms simulation')
+    #parser.add_argument('--nreps', type=int, required=True, help='number of replicates used in ms simulation')
+    #parser.add_argument('--seeds', type=int, nargs=3, default=None, help='three random seed numbers used in ms simulation; default: None')
+    #parser.add_argument('--ref-index', type=int, dest='ref_index', required=True, help='index of the reference population in the demographic model (start from 1)')
+    #parser.add_argument('--ref-size', type=int, dest='ref_size', required=True, help='sample size (haploid) of the reference population')
+    #parser.add_argument('--tgt-index', type=int, dest='tgt_index', required=True, help='index of the target population in the demographic model (start from 1)')
+    #parser.add_argument('--tgt-size', type=int, dest='tgt_size', required=True, help='sample size (haploid) of the target population')
+    #parser.add_argument('--mut-rate', type=float, dest='mut_rate', required=True, help='mutation rate per generation per base')
+    #parser.add_argument('--rec-rate', type=float, dest='rec_rate', required=True, help='recombination rate per generation per base')
+    #parser.add_argument('--seq-len', type=int, dest='seq_len', required=True, help='length of simulated sequence')
+    #parser.add_argument('--snp-num-range', type=int, dest='snp_num_range', nargs=3, required=True, help='range of SNP numbers in ms simulation; the first parameter is the minimum SNP number, the second parameter is the maximum SNP number, the third parameter is the step size')
+    #parser.add_argument('--output-dir', type=str, dest='output_dir', required=True, help='directory for the output files')
+    #parser.add_argument('--thread', type=int, default=1, help='number of thread')
+    #parser.set_defaults(runner=_run_quantile)
     
     # Arguments for threshold subcommand
-    parser = subparsers.add_parser('threshold', help='calculate S* thresholds from simulated data')
-    _add_score_args(parser)
-    parser.add_argument('--sim-data', type=str, dest='sim_data', required=True, help='name of the file containing simulated data for building a generalized additive model')
-    parser.add_argument('--recomb-rate', type=float, dest='recomb_rate', default=1e-8, help='a uniform recombination rate used across the genome; default: 1e-8')
-    parser.add_argument('--recomb-map', type=str, dest='recomb_map', default=None, help='a recombination map used across the genome; default: None')
-    parser.add_argument('--quantile', type=float, required=True, default=0.99, help='a quantile for determining significant S* scores; default: 0.99')
-    parser.add_argument('--output', type=str, required=True, help='name of the output file')
-    parser.add_argument('--k', type=int, default=8, help='dimension(s) of the bases used to represent the smooth term')
-    parser.set_defaults(runner=_run_threshold)
+    #parser = subparsers.add_parser('threshold', help='calculate S* thresholds from simulated data')
+    #_add_score_args(parser)
+    #parser.add_argument('--sim-data', type=str, dest='sim_data', required=True, help='name of the file containing simulated data for building a generalized additive model')
+    #parser.add_argument('--recomb-rate', type=float, dest='recomb_rate', default=1e-8, help='a uniform recombination rate used across the genome; default: 1e-8')
+    #parser.add_argument('--recomb-map', type=str, dest='recomb_map', default=None, help='a recombination map used across the genome; default: None')
+    #parser.add_argument('--quantile', type=float, required=True, default=0.99, help='a quantile for determining significant S* scores; default: 0.99')
+    #parser.add_argument('--output', type=str, required=True, help='name of the output file')
+    #parser.add_argument('--k', type=int, default=8, help='dimension(s) of the bases used to represent the smooth term')
+    #parser.set_defaults(runner=_run_threshold)
 
     # Arguments for matchpct subcommand
-    parser = subparsers.add_parser('matchrate', help='calculate source match rates in target populations with genomes from source populations')
-    parser.add_argument('--vcf', type=str, dest='vcf', required=True, help='name of the VCF file containing genotypes from samples')
-    _add_ref_ind_args(parser)
-    _add_tgt_ind_args(parser)
-    _add_src_ind_args(parser)
-    _add_common_args(parser)
-    _add_mapped_args(parser)
-    _add_score_args(parser)
-    parser.set_defaults(runner=_run_match_pct)
+    #parser = subparsers.add_parser('matchrate', help='calculate source match rates in target populations with genomes from source populations')
+    #parser.add_argument('--vcf', type=str, dest='vcf', required=True, help='name of the VCF file containing genotypes from samples')
+    #_add_ref_ind_args(parser)
+    #_add_tgt_ind_args(parser)
+    #_add_src_ind_args(parser)
+    #_add_common_args(parser)
+    #_add_mapped_args(parser)
+    #_add_score_args(parser)
+    #parser.set_defaults(runner=_run_match_pct)
 
     # Arguments for tract subcommand
-    parser = subparsers.add_parser('tract', help='get candidate introgressed fragments')
-    parser.add_argument('--threshold', type=str, required=True, help='threshold file from `sstar threshold`')
-    parser.add_argument('--match-rate', type=str, default=None, dest='match_pct', nargs='+', action=required_length(1,2), help='match rate files from `sstar matchrate`; the first file contains match percents using genomes from the source population 1 (src1), the second file contains match percents using genomes from the source population 2 (src2)')
-    parser.add_argument('--output-prefix', type=str, dest='output', required=True, help='prefix for output files')
-    parser.add_argument('--diff', type=float, default=0, help='difference between src1 match rates (src1_match_rate) and src2 match rates (src2_match_rate); if src1_match_rate - src2_match_rate > diff, then this fragment is assigned to src1, if src1_match_rate - src2_match_rate < diff, then this fragment is assigned to src2; default: 0')
-    parser.set_defaults(runner=_run_tract)
+    #parser = subparsers.add_parser('tract', help='get candidate introgressed fragments')
+    #parser.add_argument('--threshold', type=str, required=True, help='threshold file from `sstar threshold`')
+    #parser.add_argument('--match-rate', type=str, default=None, dest='match_pct', nargs='+', action=required_length(1,2), help='match rate files from `sstar matchrate`; the first file contains match percents using genomes from the source population 1 (src1), the second file contains match percents using genomes from the source population 2 (src2)')
+    #parser.add_argument('--output-prefix', type=str, dest='output', required=True, help='prefix for output files')
+    #parser.add_argument('--diff', type=float, default=0, help='difference between src1 match rates (src1_match_rate) and src2 match rates (src2_match_rate); if src1_match_rate - src2_match_rate > diff, then this fragment is assigned to src1, if src1_match_rate - src2_match_rate < diff, then this fragment is assigned to src2; default: 0')
+    #parser.set_defaults(runner=_run_tract)
 
-    # Arguments for archie subcommand
-    parser = subparsers.add_parser('archie', help='calculate statistics used in ArchIE')
-    parser.set_defaults(runner=_run_archie)
+    # Arguments for the simulate subcommand
+    parser = subparsers.add_parser('simulate', help='Simulate data for training.')
+    parser.set_defaults(runner=_run_simulation)
+
+    # Arguments for the train subcommand
+    parser = subparsers.add_parser('train', help='Train a statistical/machine learning model.')
+    parser.set_defaults(runner=_run_training)
+
+    # Arguments for the infer subcommand
+    parser = subparsers.add_parser('infer', help='Infer ghost introgressed fragments with a given statistical/machine learning model.')
+    parser.set_defaults(runner=_run_inference)
 
     return top_parser
 
