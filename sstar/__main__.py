d188e98d534c0b1b0a7d06e01357fba0c3aa5b15
xin-huang <xinhuang.res@gmail.com>
2023-11-08 20:41:20 +0100

    Update __main__ & train

--- sstar/__main__.py
+++ sstar/__main__.py
@@ -58,8 +58,8 @@ def _run_simulation(args):
 def _run_training(args):
     from sstar.train import train
     train(nrep=args.replicate, seq_len=args.seq_len, thread=args.thread, model_file=args.model_file, 
-          training_data_prefix=args.training_data_prefix, training_data_dir=args.training_data_dir, 
-          match_bonus=args.match_bonus, max_mismatch=args.max_mismatch, mismatch_penalty=args.mismatch_penalty, 
+          training_data_prefix=args.training_data_prefix, training_data_dir=args.training_data_dir,
+          feature_config=args.features, is_phased=args.phased, ploidy=args.ploidy,
           archaic_prop=args.intro_prop, not_archaic_prop=args.not_intro_prop, algorithm=args.model)
 
 
@@ -229,17 +229,16 @@ def _s_star_cli_parser():
 
     # Arguments for the train subcommand
     parser = subparsers.add_parser('train', help='Training a statistical/machine learning model.')
-    parser.add_argument('--vcf', type=str, required=True, help='Name of the VCF file containing genotypes from samples.')
     parser.add_argument('--seq-len', type=int, required=True, help="Length of the simulated genomes.", dest='seq_len')
     parser.add_argument('--training-data-prefix', type=str, required=True, help="Prefix of the training data file name.", dest='training_data_prefix')
     parser.add_argument('--training-data-dir', type=str, required=True, help="Directory of the training data.", dest='training_data_dir')
     parser.add_argument('--model-file', type=str, required=True, help="The file storing the trained model.", dest='model_file')
     parser.add_argument('--model', type=str, default=None, help="Statistical/machine learning model for the training. Implemented models: extra_trees, logistic_regression, sstar.")
+    parser.add_argument('--features', type=str, default=None, help='Name of the YAML file specifying what features should be used. Default: None.')
+    parser.add_argument('--phased', action='store_true', help="Enable to use phased genotypes. Default: False.")
+    parser.add_argument('--ploidy', type=int, default=2, help='Ploidy of genomes. Default: 2.')
     parser.add_argument('--introgressed-prop', type=float, default=0.7, help="Proportion that determines a fragment as introgressed. Default: 0.7.", dest="intro_prop")
     parser.add_argument('--not-introgressed-prop', type=float, default=0.3, help="Proportion that determinse a fragment as non-introgressed. Default: 0.3.", dest="not_intro_prop")
-    parser.add_argument('--match-bonus', type=int, default=5000, help='Bonus for matching genotypes of two different variants when calculating the S* statistic. Default: 5000.', dest='match_bonus')
-    parser.add_argument('--max-mismatch', type=int, default=5, help='Maximum genotype distance allowed when calculating the S* statistic. Default: 5.', dest='max_mismatch')
-    parser.add_argument('--mismatch-penalty', type=int, default=-10000, help='Penalty for mismatching genotypes of two different variants when calculating the S* statistic. Default: -10000.', dest='mismatch_penalty')
     parser.add_argument('--replicate', type=int, required=True, help="Number of replications in the training data.")
     parser.add_argument('--thread', type=int, default=1, help="Number of threads for the training. Default: 1.")
     parser.set_defaults(runner=_run_training)
